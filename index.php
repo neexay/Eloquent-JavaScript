bb4126882d9ba4461f07253f4d96e30a72678c18
Roman Shapovalov IT-technologies.us <info@it-technologies.us>
2021-09-09 19:57:08 +0300

    первая задача

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Треугольник в цикле</title>
+</head>
+<body>
+<h1></h1>
+<script src="index.js" type="text/javascript"></script>
+</body>
+</html>
