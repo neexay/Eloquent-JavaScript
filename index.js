4b5fdfdc59a5a3f5525e8dcb3e5b495330b9284c
neexay <woio@bk.ru>
2022-11-24 21:10:14 +0300

    задачи первой главы

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-
-let result="#";
-for (let i=0;i<=7;i++){
-    console.log(result);
-    result+="#";
+//Решение первой задачи
+document.getElementById("triangle").onclick =function () {
+    let result="#";
+    for (let i=0;i<=7;i++){
+        console.log(result);
+        result+="#";
+    }
 }
+//----------------------------------------------------------------------------------------------------------------------
+//Решение второй задачи
+document.getElementById("FizzBuzz").onclick =function () {
+    for (let i=1;i<=100;i++){
+        let result="";
+        if (i % 3===0){
+           result+="Fizz";
+        }
+        if(i % 5===0){
+           result+="Buzz";
+        }
+        if (result.length<1){
+            console.log(i);
+        }else {
+            console.log(result);
+        }
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------
+//Решение третей задачи
+document.getElementById("chess").onclick =function () {
+    let result="";
+    for (let i=0;i<=7;i++){
+       for (let j=0;j<=7;j++){
+         if ((j+i)%2===0){
+             result+="#";
+         }else {
+             result+=" ";
+         }
+       }
+        result+="\n";
+    }
+    console.log(result);
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
